e0a7c3cd9894cbf9b2b21f33c13490708febdb45
Mazart23 <mazurkiewiczaartur@gmail.com>
2025-06-02 19:50:57 +0200

    remove thread for debug

--- controller/app.py
+++ controller/app.py
@@ -1,10 +1,6 @@
-import threading
-
 from src import app
-from src.utils.algorithm import load_graph
 from src.utils.apps import Services
 
 
 if __name__ == '__main__':
-    threading.Thread(target=load_graph, daemon=True).start()
     app.run(host="0.0.0.0", port=Services.CONTROLLER.port, debug=True)
