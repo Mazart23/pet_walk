21acea1f0ad58806f82c2e2621c7004fecdf3915
Mazart23 <mazurkiewiczaartur@gmail.com>
2025-06-02 20:41:09 +0200

    without gunicorn

--- controller/app.py
+++ controller/app.py
@@ -1,6 +1,10 @@
+import threading
+
 from src import app
+from src.utils.algorithm import load_graph
 from src.utils.apps import Services
 
 
 if __name__ == '__main__':
+    threading.Thread(target=load_graph, daemon=True).start()
     app.run(host="0.0.0.0", port=Services.CONTROLLER.port, debug=True)
